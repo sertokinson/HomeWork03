9a3165f4cc08861b4d70e7ea6e4c1ff962de5de1
Sergey Stakhanov <sertokinson@gmail.com>
2017-11-07 14:52:06 +0300

    изменены тесты

--- src/test/java/Task2Test.java
+++ src/test/java/Task2Test.java
@@ -7,22 +7,9 @@ import java.io.IOException;
 public class Task2Test extends Assert {
     @Test
     public void test() throws IOException {
-        File f = new File("C:\\JavaSchool\\Homeworks\\HomeWork_03\\newVersion\\collections\\text.txt");
-        String[]strings=new String[]{
-                "m",
-                "Aa",
-                "df",
-                "sd",
-                "efr",
-                "fdg",
-                "sdf",
-                "rmjd",
-                "sdas",
-                "sdasd"
-        };
-        int i=0;
-        for (String s:Task2.differentString(f))
-            assertEquals(s,strings[i++]);
+        assertArrayEquals(Task2.differentString(
+                new File("C:\\JavaSchool\\Homeworks\\HomeWork_03\\newVersion\\collections\\text.txt")).toArray(),
+                new String[]{"m", "Aa", "df", "sd", "efr", "fdg", "sdf", "rmjd", "sdas", "sdasd"});
     }
 
 }
--- src/test/java/Task4Test.java
+++ src/test/java/Task4Test.java
@@ -7,18 +7,9 @@ import java.io.IOException;
 public class Task4Test extends Assert {
     @Test
     public void test() throws IOException {
-        File f = new File("C:\\JavaSchool\\Homeworks\\HomeWork_03\\newVersion\\collections\\text.txt");
-        String[] strings = new String[]{
-                "df343efr",
-                "",
-                "df3fdg",
-                "sdasd23sd412rmjd",
-                "sdf4sd5,.,m.sdas5"
-        };
-        int i = 0;
-        for (String s : Task4.reverceLine(f)) {
-            assertEquals(s, strings[i++]);
-        }
+        Assert.assertArrayEquals(Task4.reverceLine(
+                new File("C:\\JavaSchool\\Homeworks\\HomeWork_03\\newVersion\\collections\\text.txt")).toArray(),
+                new String[]{"df343efr2df", "", "df3fdg", "sdasd2Aa3sd412rmjd", "sdf4sd5,.,m.sdas5"});
     }
 
 }
